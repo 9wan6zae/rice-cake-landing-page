e447bccc72146f4a1d45ee3abebb2143de359f7d
한광재 <hkj4900@pusan.ac.kr>
2022-02-26 17:19:34 +0900

    Feat: 헤더와 푸터 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import AppFooter from '../components/appFooter'
+import AppHeader from '../components/appHeader'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AppHeader />
+      <Component {...pageProps} />
+      <AppFooter />
+    </>
+  )
 }
 
 export default MyApp
